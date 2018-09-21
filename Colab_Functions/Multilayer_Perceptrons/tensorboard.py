c6362dae9753fac432682e06b04409cfa1a3ef10
Michael A. Teti <mtetifau@gmail.com>
2018-09-21 13:14:19 -0400

    Update tensorboard.py

--- Colab_Functions/Multilayer_Perceptrons/tensorboard.py
+++ Colab_Functions/Multilayer_Perceptrons/tensorboard.py
@@ -1,13 +1,22 @@
 def install_tensorboard_dep():
+  '''This function installs the necessary software to get tensorboard
+     working on colaboratory.'''
+  
   if 'ngrok-stable-linux-amd64.zip' not in os.listdir(os.getcwd()):
     !wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
     !unzip ngrok-stable-linux-amd64.zip
     os.system('n')
+    
+  return
 
 
 def start_tensorboard():
+  '''This function starts tensorboard so you can visualize training.'''
+  
   LOG_DIR = '/tmp/tflearn_logs'
   get_ipython().system_raw('tensorboard --logdir {} --host 0.0.0.0 --port 6006 &'.format(LOG_DIR))
   get_ipython().system_raw('./ngrok http 6006 &')
   ! curl -s http://localhost:4040/api/tunnels | python3 -c \
   "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
+  
+  return
